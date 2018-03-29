ad10a5c0beaeb1dbc5bfdea098a5025b4242b6bf
Colton Allen <cmanallen90@gmail.com>
2018-03-29 09:50:07 -0700

    Improve setup script

--- setup.py
+++ setup.py
@@ -1,8 +1,29 @@
-from setuptools import setup
+"""resourceful."""
+from setuptools import find_packages, setup
 
 
 setup(
-    name='resourceful',
-    version='0.1',
-    py_modules=['resourceful'],
+    name='jsonapi-query',
+    version='0.2',
+    url='https://github.com/caxiam/resourceful',
+    license='Apache Version 2.0',
+    author='Colton Allen',
+    author_email='colton.allen@caxiam.com',
+    long_description=__doc__,
+    packages=find_packages(exclude=("test*", )),
+    package_dir={'resourceful': 'resourceful'},
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    test_suite='tests'
 )
